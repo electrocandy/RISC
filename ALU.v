8f595c9c362c7aa8082af5201259c210fd228889
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:12:06 +0530

    Updated

--- ALU.v
+++ ALU.v
@@ -1,14 +1,16 @@
-module ALU #(parameter N=32)(input [N-1:0]A,input [N-1:0]B,input [3:0]control,output reg [N-1:0]out,output reg zero);
+module ALU #(parameter N=32)(input [N-1:0]A,input [N-1:0]B,input [3:0]control,output [N-1:0]out,output zero);
 
+reg [N-1:0]outbuff=0;
 always@(*) begin
 case(control)
-4'b0000: out<=A&B;
-4'b0001: out<=A|B;
-4'b0010: out<=A+B;
-4'b0110: out<=A-B;
+4'b0000: outbuff<=A&B;
+4'b0001: outbuff<=A|B;
+4'b0010: outbuff<=A+B;
+4'b0110: outbuff<=A-B;
 endcase
-zero=(out==0)? 1:0;
 end
 
+assign zero=(out==0)? 1:0;
+assign out=outbuff;
 
 endmodule
