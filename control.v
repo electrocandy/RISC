642e6e42531318decf11814a525777aa26820cde
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:35:39 +0530

    added controls

--- control.v
+++ control.v
@@ -1,6 +1,17 @@
 module control #(parameter N=32)
-(input [6:0]instr,output branch,output memread,output memtoreg,output [1:0]ALUop,output memwrite,output ALUsrc,output regwrite);
+(input [6:0]instr,input clk,output branch,output memread,output memtoreg,output [1:0]ALUop,output memwrite,output ALUsrc,output regwrite);
 
 reg [7:0] out;
 always@(*)begin
-7'b0110011: out=8'b;
+case(instr[6:0])
+7'b0110011: out=8'b00010001; //R-type //Here both memreg and memread are zero as memory is used only during load or store operations
+7'b0000011: out=8'b01100011; //I-type
+7'b0100011: out=8'b00x00110; //S-type
+7'b1100011: out=8'b10x01000; //B_type(BEQ)
+default: out=8'bx;
+endcase
+end
+
+assign {branch,memread,memtoreg,ALUop,memwrite,ALUsrc,regwrite}=out; 
+
+endmodule
