5a16d23588f8d0d2975fdfb8cebcf6058eda087a
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:40:07 +0530

    merged pc and mux

--- programcounter.v
+++ programcounter.v
@@ -1,7 +1,22 @@
-module program_counter #(parameter N=32)(input [N-1:0] pcin,input clk,output reg [N-1:0]pcout);
+module program_counter #(parameter N=32)(input branch, input zero,input [N-1:0]imm,input clk,output [N-1:0]pcout);
 
-always@(posedge clk) begin
-pcout<=pcin;
+reg [N-1:0]pcoutbuff;
+initial 
+$display("Simulation started");
+
+initial begin
+pcoutbuff=0;
 end
+
+always@(posedge clk)begin
+if(branch==1'b1 && zero==1'b1) begin
+   pcoutbuff<=pcoutbuff+imm;
+   //SB Type instructions not implemented in terms of byte offset but directly in terms of instruction 
+   end
+else begin
+   pcoutbuff<=pcoutbuff+1;
+   end
+end
+assign pcout=pcoutbuff;
 
 endmodule
