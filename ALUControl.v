fd1e9bdf09d70a9c2cc86c764fd9c0c1207982a7
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:34:03 +0530

    Fixed issues

--- ALUControl.v
+++ ALUControl.v
@@ -1,18 +1,22 @@
-module ALUControl(input [1:0]ALUOp,input [3:0]funct3,input funct7,output reg [3:0]control);
+module ALUControl(input [1:0]ALUOp,input [2:0]funct3,input funct7,output [3:0]control);
 
+reg [3:0]contbuff=0;
 always@(*)
 begin
 case(ALUOp)
-2'b00: control<=4'b0010;
-2'b01: control<=4'b0110;
+2'b00: contbuff<=4'b0010;
+2'b01: contbuff<=4'b0110;
 2'b10: case({funct7,funct3})
-       4'b0000: control<=4'b0010;
-       4'b1000: control<=4'b0110;
-       4'b0111: control<=4'b0000;
-       4'b0110: control<=4'b0001;
+       4'b0000: contbuff<=4'b0010;
+       4'b1000: contbuff<=4'b0110;
+       4'b0111: contbuff<=4'b0000;
+       4'b0110: contbuff<=4'b0001;
 
-default: control<=4'bxxxx;
+default: contbuff<=4'bxxxx;
 endcase
 endcase
 end
+
+assign control=contbuff;
+
 endmodule
