0e7c35a50801fe2fa09d9e27ae9fd4637ce88e54
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:36:46 +0530

    added fixes

--- data_memory.v
+++ data_memory.v
@@ -1,13 +1,14 @@
 module data_memory #(parameter N=32,parameter M=256)
-(input [N-1:0]address,input [N-1:0]data_input,input clk,input memread,input memwrite,output reg [N-1:0]data);
+(input [N-1:0]address,input [N-1:0]data_input,input clk,input memread,input memwrite,output [N-1:0]data);
 
-//register file needs memread signal as reading invalid address causes issues
+//data memory needs memread signal as reading invalid address causes issues
 
 reg [N-1:0] mem[M-1:0];
+reg [N-1:0] databuf=0;
 integer i;
 initial begin
 for(i=0;i<M;i=i+1)
-   mem[i]=32'b0;
+   mem[i]=32'b1;
 end
 
 always@(posedge clk) begin
@@ -16,8 +17,10 @@ if(memwrite) //memory write is performed always at clock edge in data memory
 end
 
 always@(*) begin
-if(memread)  //memory read is performed for any change in sensitivity list
-  data=mem[address];
+if(memread)  //memory read is performed for any change in sensitivity list(inputs)
+  databuf=mem[address];
 end
+
+assign data=databuf;
 
 endmodule
