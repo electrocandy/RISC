ce1e33cbacea415bceaca23f04f7b644f178a8ff
Anirudhan <anirudhans2004@gmail.com>
2024-09-23 01:41:44 +0530

    added fixes

--- mux.v
+++ mux.v
@@ -1,3 +1,3 @@
-module mux(input a,input b,input sel,output y);
-assign y=(sel)?a:b;
+module mux #(parameter N=32)(input [N-1:0]a,input [N-1:0]b,input sel,output [N-1:0]y);
+assign y=(sel==1'b1)?a:b;
 endmodule
